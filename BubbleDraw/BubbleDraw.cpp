069225fbb0b644ae8d7486224a2a870ee8fb6144
John Apt <jwa824@hotmail.com>
2023-02-09 14:57:22 -0600

    First commit.

--- BubbleDraw/BubbleDraw.cpp
+++ BubbleDraw/BubbleDraw.cpp
@@ -1,20 +1,121 @@
-// BubbleDraw.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <SDL.h>
+#include <stdio.h>
+#include <vector>
+#undef main
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+void DrawCircle(SDL_Renderer* renderer, int32_t centreX, int32_t centreY, int32_t radius);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	SDL_Window* window = nullptr;
+	SDL_Renderer* renderer = nullptr;
+	SDL_Event e;
+	SDL_Init(SDL_INIT_EVERYTHING);
+	bool running = true;
+	SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, 0, &window, &renderer);
+
+	//Set draw color to black and clear screen
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+
+	int xMouse = 0;
+	int yMouse = 0;
+	int radius = 100;
+	
+	while (running)
+	{
+		while (SDL_PollEvent(&e))
+		{
+			if (e.type == SDL_QUIT)
+			{
+				running = false;
+			}
+			else if (e.type == SDL_KEYDOWN)
+			{
+				switch (e.key.keysym.sym)
+				{
+				case SDLK_ESCAPE:
+					running = false;
+					break;
+				case SDLK_c:
+					DrawCircle(renderer, xMouse, yMouse, radius);
+					break;
+				}
+			}
+			else if (e.type == SDL_KEYUP)
+			{
+				
+			}
+			else if (e.type == SDL_MOUSEMOTION)
+			{
+				SDL_GetMouseState(&xMouse, &yMouse);
+			}
+			else if (e.type == SDL_MOUSEWHEEL)
+			{
+				if (e.wheel.y > 0)
+				{
+					radius += 10;
+					std::cout << "Radius: " << radius << std::endl;
+				}
+				else if (e.wheel.y < 0)
+				{
+					if (radius > 10)
+						radius -= 10;
+					std::cout << "Radius: " << radius << std::endl;
+				}
+			}
+		}
+
+		
+		//Set draw color to white
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+		//Draw circle
+		DrawCircle(renderer, 320, 240, 100);
+
+		SDL_RenderPresent(renderer);
+	}
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void DrawCircle(SDL_Renderer* renderer, int32_t centreX, int32_t centreY, int32_t radius)
+{
+	const int32_t diameter = (radius * 2);
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	int32_t x = (radius - 1);
+	int32_t y = 0;
+	int32_t tx = 1;
+	int32_t ty = 1;
+	int32_t error = (tx - diameter);
+
+	while (x >= y)
+	{
+		//  Each of the following renders an octant of the circle
+		SDL_RenderDrawPoint(renderer, centreX + x, centreY - y);
+		SDL_RenderDrawPoint(renderer, centreX + x, centreY + y);
+		SDL_RenderDrawPoint(renderer, centreX - x, centreY - y);
+		SDL_RenderDrawPoint(renderer, centreX - x, centreY + y);
+		SDL_RenderDrawPoint(renderer, centreX + y, centreY - x);
+		SDL_RenderDrawPoint(renderer, centreX + y, centreY + x);
+		SDL_RenderDrawPoint(renderer, centreX - y, centreY - x);
+		SDL_RenderDrawPoint(renderer, centreX - y, centreY + x);
+
+		if (error <= 0)
+		{
+			++y;
+			error += ty;
+			ty += 2;
+		}
+
+		if (error > 0)
+		{
+			--x;
+			tx += 2;
+			error += (tx - diameter);
+		}
+	}
+}
